7ffe1ca3f6f894d9670627689532f2576a6dd096
rahye.kim <rhhhkh@gmail.com>
2024-06-15 15:00:07 +0900

    echo빼먹음

--- dags/dags_bash_operator_standard.py
+++ dags/dags_bash_operator_standard.py
@@ -11,7 +11,7 @@ my_dag=DAG(
 )
 
 BashOperator(task_id="bash_t1", 
-             bash_command="whoami", 
+             bash_command="echo whoami", 
              dag=my_dag)
 BashOperator(task_id="bash_t2", 
              bash_command="echo $HOSTNAME",
