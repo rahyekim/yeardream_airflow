32677d391c1584b2c3bc97cba0d12fe45e7f322d
rahye.kim <rhhhkh@gmail.com>
2024-06-18 01:09:34 +0900

    이주전 macro.dateutil.relativedelta

--- dags/dags_bash_macro_eg2.py
+++ dags/dags_bash_macro_eg2.py
@@ -17,13 +17,6 @@ with DAG(
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
     
-    bash_task_3= BashOperator(
-        task_id= 'bash_task_3',
-        env={'START_DATE':'{{ (data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=5)) }}',
-             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") -macros.dateutil.relativedelta.relativedelta(weeks=2) | ds}}'
-        },
-        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
-    )
         
     
-    bash_task_2 >> bash_task_3
+    bash_task_2
