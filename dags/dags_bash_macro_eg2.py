77e55c74602b14821d95318490f580e5f63187d6
rahye.kim <rhhhkh@gmail.com>
2024-06-18 01:30:07 +0900

    괄호하나더해서 오류남

--- dags/dags_bash_macro_eg2.py
+++ dags/dags_bash_macro_eg2.py
@@ -11,7 +11,7 @@ with DAG(
     # start_date= 2주전 월요일 / end_date=2주전 토요일 (YYYY-MM-DD)
     bash_task_2= BashOperator(
         task_id= 'bash_task_2',
-        env={'START_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) }}',
+        env={'START_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19) }}',
              'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") -macros.dateutil.relativedelta.relativedelta(days=14) | ds}}'
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
