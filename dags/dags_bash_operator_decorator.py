47f58f7c284b42ffb29f5ab4c907936729c06640
rahye.kim <rhhhkh@gmail.com>
2024-06-17 09:42:55 +0900

    email 오퍼레이더

--- dags/dags_bash_operator_decorator.py
+++ dags/dags_bash_operator_decorator.py
@@ -1,6 +1,6 @@
 import datetime
 import pendulum
-from airflow.decorators import dag
+from airflow.decorators import dag 
 from airflow.operators.bash import BashOperator
 
 @dag(
@@ -8,7 +8,7 @@ from airflow.operators.bash import BashOperator
     schedule="0 13 * * 5#2",
     start_date=pendulum.datetime(2024,5,1, tz="Asia/Seoul"),
     catchup=False,
-    tags=["homework"]
+    tags=["homework"] 
 )
 
 def my_task():
@@ -21,7 +21,7 @@ def my_task():
         task_id="bash_t2",
         bash_command="echo $HOSTNAME",
 )
-    bash_t1 >> bash_t2
+    bash_t1 >> bash_t2  
     
 my_task()
 
